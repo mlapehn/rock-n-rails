f573b4160785b125f959d93853e4932e472bb13c
Matthew John Lapehn <mlapehn@gmail.com>
2016-07-26 15:56:02 -0600

    Completed Rock-N-Rails

--- records/app/controllers/records_controller.rb
+++ records/app/controllers/records_controller.rb
@@ -12,5 +12,15 @@ class RecordsController < ApplicationController
 		@record = Record.new
     	render :new #optional
   	end
+  	def create
+   		 Record.create(record_params)
+   		 redirect_to('/records')
+  	end
+
+  private
+
+  def record_params
+   	 params.require(:record).permit(:title, :artist, :year, :cover_art, :song_count)
+  end
 
 end
--- records/config/routes.rb
+++ records/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	  resources :records, only: [:index, :new, :show]
+	  resources :records, except: [:delete, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
